d7daf357e9a109e7def76cb295f5ef6329e6e570
Michael Siu <michaeltsiu13@gmail.com>
2020-07-13 11:44:49 -0700

    Query Strings

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import Results from './Results';
-import { ThemeConsumer } from '../contexts/theme'
+import { ThemeConsumer } from '../contexts/theme';
+import { Link } from 'react-router-dom';
 
 function Instructions() {
   return (
@@ -132,8 +133,7 @@ export default class Battle extends React.Component {
     super(props)
     this.state = {
       playerOne: null,
-      playerTwo: null,
-      battle: false
+      playerTwo: null
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
@@ -152,20 +152,8 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo, battle } = this.state
+    const { playerOne, playerTwo } = this.state
 
-    if (battle === true) {
-      return (
-        <Results
-          playerOne={playerOne}
-          playerTwo={playerTwo}
-          onReset={() => this.setState({
-            playerOne: null,
-            playerTwo: null,
-            battle: false
-          })}
-        />)
-    }
     return (
       <React.Fragment>
         <Instructions />
@@ -197,12 +185,15 @@ export default class Battle extends React.Component {
             }
           </div>
           {playerOne && playerTwo && (
-            <button
+            <Link
               className='btn dark-btn btn-space'
-              onClick={() => this.setState({ battle: true })}
+              to={{
+                pathname: '/battle/results',
+                search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+              }}
             >
               Battle
-            </button>
+            </Link>
           )}
         </div>
       </React.Fragment>
